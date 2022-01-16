19b851b5c3168cb97266444a7079e3bd6eece6d9
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-16 16:53:21 +0100

    fix: replacedLast \n to avoid code

--- components/MDX/Code.js
+++ components/MDX/Code.js
@@ -1,13 +1,29 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
-export function Code({ className, ...props }) {
+export function Code({ className, children, ...props }) {
   const match = /language-(\w+)/.exec(className || "");
   const language = match && match[1];
 
   if (!language) {
-    return <code className={className} {...props} />;
+    return (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
   }
+
+  const replaceLast = (str, find, replace) => {
+    const index = str.lastIndexOf(find);
+    if (index === -1) {
+      return str;
+    }
+    return (
+      str.substring(0, index) + replace + str.substring(index + find.length)
+    );
+  };
+
+  children = replaceLast(children, "\n", "");
 
   return (
     <SyntaxHighlighter
@@ -26,6 +42,8 @@ export function Code({ className, ...props }) {
       wrapLines={true}
       showLineNumbers={true}
       {...props}
-    />
+    >
+      {children}
+    </SyntaxHighlighter>
   );
 }
