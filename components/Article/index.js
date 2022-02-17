becbc7c24109a73ee2beb1995faa8280db274aa0
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-02-17 16:35:47 +0100

    feat: added links to article

--- components/Article/index.js
+++ components/Article/index.js
@@ -1,7 +1,8 @@
+import NextLink from "next/link";
 import { styled } from "../../stitches.config";
-import { Subheader } from "../UI/Subheader";
-import { Paragraph } from "../UI/Paragraph";
 import { Flex } from "../UI/Flex";
+import { Paragraph } from "../UI/Paragraph";
+import { Subheader } from "../UI/Subheader";
 
 const ArticleIcon = styled("img", {
   height: "22px",
@@ -14,19 +15,25 @@ const Excerpt = styled(Paragraph, {
   color: "$blue500",
 });
 
-export const Article = ({ title, description }) => {
+export const Article = ({ id, title, description }) => {
   const excerpt = description.split(" ").slice(0, 10).join(" ") + "...";
 
   return (
-    <Flex as="a" href="#" direction="row" css={{ marginTop: "2rem" }}>
-      <ArticleIcon
-        src={"/assets/icons/javascript.svg"}
-        css={{ marginTop: "4px", marginRight: "0.4rem" }}
-      />
-      <Flex direction="column" align="stretch">
-        <Title>{title}</Title>
-        <Excerpt>{excerpt}</Excerpt>
+    <NextLink href="/articles/[id]" as={`/articles/${id}`}>
+      <Flex
+        as="a"
+        direction="row"
+        css={{ marginTop: "2rem", cursor: "pointer" }}
+      >
+        <ArticleIcon
+          src={"/assets/icons/javascript.svg"}
+          css={{ marginTop: "4px", marginRight: "0.4rem" }}
+        />
+        <Flex direction="column" align="stretch">
+          <Title>{title}</Title>
+          <Excerpt>{excerpt}</Excerpt>
+        </Flex>
       </Flex>
-    </Flex>
+    </NextLink>
   );
 };
