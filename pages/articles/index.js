3c9de4928ae49fb45360ba0ba96d00bd6baa5891
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-02-20 21:01:34 +0100

    feat: added articles page

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,5 +1,7 @@
+import NextLink from "next/link";
 import { Flex, Text } from "../../components/UI";
 import { getArticleData, getArticles } from "../../lib/utils/articles";
+import { styled } from "../../stitches.config";
 
 export async function getStaticProps() {
   const articles = [];
@@ -23,21 +25,58 @@ export async function getStaticProps() {
   };
 }
 
-export const Article = ({ title, description }) => {
+const ArticleContainer = styled(Flex, {
+  position: "relative",
+
+  "&::before": {
+    content: "",
+    borderRadius: "8px",
+    display: "block",
+    position: "absolute",
+    zIndex: "0",
+    inset: "calc(15px*-1)",
+    padding: "15px",
+    opacity: "0",
+    transform: "scale(0.9)",
+  },
+
+  "&:focus": {
+    "&::before": {
+      outline: "none",
+      opacity: "1",
+      boxShadow: "inset 0 0 0 2px $blue200",
+      transition: "all 0.2s",
+      transform: "scale(1)",
+    },
+  },
+
+  "&:hover": {
+    "&::before": {
+      opacity: "1",
+      background: "$white030",
+      transition: "all 0.2s",
+      transform: "scale(1)",
+    },
+  },
+});
+
+export const Article = ({ id, title, description }) => {
   const excerpt = description.split(" ").slice(0, 20).join(" ") + "...";
 
   return (
-    <Flex direction="column" gap="4">
-      <Flex direction="column" gap="2">
-        <Text size="1" color="tertiary" css={{ fontSize: "0.9rem" }}>
-          08 September 2021
+    <NextLink href="/articles/[id]" as={`/articles/${id}`} passHref>
+      <ArticleContainer as="a" direction="column" gap="4">
+        <Flex direction="column" gap="2">
+          <Text size="1" color="tertiary" css={{ fontSize: "0.9rem" }}>
+            08 September 2021
+          </Text>
+          <Text size="3">{title}</Text>
+        </Flex>
+        <Text size="1" color="secondary" css={{ maxWidth: "65ch" }}>
+          {excerpt}
         </Text>
-        <Text size="3">{title}</Text>
-      </Flex>
-      <Text size="1" color="secondary" css={{ maxWidth: "65ch" }}>
-        {excerpt}
-      </Text>
-    </Flex>
+      </ArticleContainer>
+    </NextLink>
   );
 };
 
@@ -45,7 +84,7 @@ export default function Home({ articles }) {
   return (
     <>
       <section>
-        <Text size="4" color="glowing" css={{ marginTop: "5rem" }}>
+        <Text size="4" color="glowing">
           Articles
         </Text>
       </section>
