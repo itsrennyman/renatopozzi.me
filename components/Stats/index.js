ad2056e8d1626879bfcdde2725e7b352f4fd06af
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-02-20 21:33:27 +0100

    feat: added dashboard page

---
+++ components/Stats/index.js
@@ -0,0 +1,18 @@
+import { Card, Flex, Text } from "../UI";
+
+export const Stats = ({ title, value }) => {
+  return (
+    <Card>
+      <Flex direction="column" gap="7" align="baseline">
+        <Flex direction="column" gap="3">
+          <Text size="3" css={{ fontWeight: 500 }}>
+            {title}
+          </Text>
+          <Text size="4" color="secondary">
+            {value}
+          </Text>
+        </Flex>
+      </Flex>
+    </Card>
+  );
+};
---
+++ pages/dashboard.js
@@ -0,0 +1,52 @@
+import { Counter } from "../components/Counter";
+import { Stats } from "../components/Stats";
+import { Grid, Text } from "../components/UI";
+
+export default function Dashboard({ articles }) {
+  return (
+    <>
+      <section>
+        <Text as="h1" color="glowing" size="4">
+          Dashboard
+        </Text>
+        <Text size="2" css={{ marginTop: "2.5rem" }}>
+          This is a personal statistics dashboard. I use this to track various
+          metrics across platforms like Unsplash.
+        </Text>
+      </section>
+
+      <section>
+        <Grid
+          columns={{ "@initial": "1", "@bp1": "2" }}
+          gap={5}
+          css={{ marginTop: "2rem" }}
+        >
+          <Stats
+            title="Github Stars"
+            value={<Counter url="/api/github" value="stars" />}
+          />
+          <Stats
+            title="Github Followers"
+            value={<Counter url="/api/github" value="followers" />}
+          />
+          <Stats
+            title="Unsplash Views"
+            value={<Counter url="/api/unsplash" value="views" />}
+          />
+          <Stats
+            title="Unsplash Downloads"
+            value={<Counter url="/api/unsplash" value="downloads" />}
+          />
+          <Stats
+            title="Twitter Followers"
+            value={<Counter url="/api/twitter" value="followers_count" />}
+          />
+          <Stats
+            title="Ether Price"
+            value={<Counter url="/api/ether" value="ethusd" />}
+          />
+        </Grid>
+      </section>
+    </>
+  );
+}
