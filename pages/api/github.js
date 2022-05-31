b558d19ce1473144e0973e23ac34e12791750cbe
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-31 13:55:41 +0200

    fix: api github

--- pages/api/github.js
+++ pages/api/github.js
@@ -7,16 +7,11 @@ const fetchRenny = async () =>
 const fetchRennyRepos = async () =>
   await (await fetch("https://api.github.com/users/itsrennyman/repos")).json();
 
-const fetchAuroraRepos = async () =>
-  await (await fetch("https://api.github.com/orgs/useaurora/repos")).json();
-
 export default async function github(req, res) {
   const user = await fetchRenny();
   const rennyRepos = await fetchRennyRepos();
-  const auroraRepos = await fetchAuroraRepos();
 
   const rennyStars = rennyRepos.filter(notFork).reduce(sumStars, 0);
-  const auroraStars = auroraRepos.filter(notFork).reduce(sumStars, 0);
 
   res.setHeader(
     "Cache-Control",
@@ -25,6 +20,6 @@ export default async function github(req, res) {
 
   return res.status(200).json({
     followers: user.followers,
-    stars: rennyStars + auroraStars,
+    stars: rennyStars,
   });
 }
