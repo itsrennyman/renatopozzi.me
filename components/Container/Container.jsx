2316922e4f34da13c91fbb1bfd0a30879b2f82bc
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-31 17:48:10 +0200

    added missing seo data

--- components/Container/Container.jsx
+++ components/Container/Container.jsx
@@ -1,7 +1,13 @@
+import { SEOData } from "../SEOData";
 import styles from "./Container.module.css";
 
-const Container = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+const Container = ({ children, seo }) => {
+  return (
+    <div className={styles.container}>
+      <SEOData {...seo} />
+      {children}
+    </div>
+  );
 };
 
 export { Container };
