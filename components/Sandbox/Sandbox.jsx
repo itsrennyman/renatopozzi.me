da1756b4a02d52ba959667e4fe7486196bca3b6a
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-06-14 18:13:30 +0200

    fix padding

--- components/Sandbox/Sandbox.jsx
+++ components/Sandbox/Sandbox.jsx
@@ -8,6 +8,7 @@ const Sandbox = ({ id }) => {
         border: 0,
         borderRadius: "4px",
         overflow: "hidden",
+        padding: "1rem 0",
       }}
       allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
       sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
