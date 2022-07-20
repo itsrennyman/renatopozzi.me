3d886545bb1ac90dcf8cee4ffe33106fa739f8bd
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-07-20 23:10:31 +0200

    added offer alert

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,24 @@ import "../styles/normalize.css";
 import "../styles/styles.css";
 import "../styles/typography.css";
 
+const OfferAlert = () => {
+  return (
+    <a
+      href="https://itsrennyman.gumroad.com/l/150-front-end-development-learning-resources/HAPPY-DEVELOPER"
+      className="p-4 block text-center text-base bg-zinc-700 text-zinc-100"
+      role="alert"
+    >
+      <span className="font-semibold">SPECIAL OFFER:</span> Obtain 150+
+      Front-End Development Resources with a{" "}
+      <span className="font-semibold">30% Discount</span>! CLICK HERE!
+    </a>
+  );
+};
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <OfferAlert />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
